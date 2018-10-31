5877eaf7c9c6be225e04e7bd083dec452c13d332
Alexandra Salois <alexasalois@hotmail.com>
2018-10-31 15:39:19 -0400

    EX6: Mistake 17= Extra "("
    Deleted the extra opening parathese in the ball script.

--- exercises/exercise6/broken-game-basic-oop-pong/js/Ball.js
+++ exercises/exercise6/broken-game-basic-oop-pong/js/Ball.js
@@ -53,14 +53,14 @@ Ball.prototype.isOffScreen = function () {
 //
 // Draw the ball as a rectangle on the screen
 Ball.prototype.display = function () {
-  rect(this.x this.y);
+  rect(this.x this.y);////////////////////////////////////////////////
 }
 
 // handleCollision(paddle)
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototype.handleCollision = function((paddle) {
+Ball.prototype.handleCollision = function(paddle) {
   // Check if the ball overlaps the paddle on x axis
   if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
     // Check if the ball overlaps the paddle on y axis
