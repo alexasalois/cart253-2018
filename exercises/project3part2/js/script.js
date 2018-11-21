147d16b6a25fe32bd13c2ac50acad2ae9f8c38b9
Alexandra Salois <alexasalois@hotmail.com>
2018-11-21 12:47:39 -0500

    P3: Part 2: Added rotating target
    target movies accross the screen and rotates around randomly.

--- exercises/project3part2/js/script.js
+++ exercises/project3part2/js/script.js
@@ -8,23 +8,26 @@
 ///////////////////////////
 
 var angle = 0;
-var rectx = 0;
-var recty = 0;
+var hellox = 0;
+var helloy = 0;
 
 function setup() {
   createCanvas(500,500);
-  recty = height/2;
+  recty = 5;
+  helloy = height/2;
 }
 
 
 function draw() {
-  background(255);
+  //background(255);
   fill(0);
-  translate(width/2,height/2);
+  translate(hellox,helloy);
   rotate(angle);
   rectMode(CENTER);
-  rect(rectx,recty,20,20);
-  angle += -0.01;
-  rectx += 1.5;
-  recty += -0.5;
+  rect(0,75,20,20);
+
+  angle += -0.1;
+  hellox += 1;
+  helloy += random(-10,10);
+  //recty += -0.5;
 }
